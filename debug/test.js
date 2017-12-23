c16fdd158f8940a4586776ec56eddaa6c86dd8a1
whiterasbk <2980725051@qq.com>
2017-12-23 12:55:35 +0800

    change testload path

--- debug/test.js
+++ debug/test.js
@@ -1,4 +1,4 @@
-var main_path = "/storage/sdcard0/JavaScriptProjects/Library/main.js";
+var main_path = "/storage/sdcard0/github Projects/rhinoLibrary/main.js";
 var main_stream = new java.io.FileInputStream(new java.io.File(main_path));
 var main_buffer = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, main_stream.available());
 main_stream.read(main_buffer);
